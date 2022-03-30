9b0ca6c09be0256943e23173180289371521943f
Sidachenko Michael <michaellux@yandex.ua>
2022-03-30 23:10:25 +0300

    generate bubbleField structure

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,8 +12,10 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance = null;
 
-    [SerializeField] private Transform playField;
+    [SerializeField] private Transform playFieldTransform;
     [SerializeField] private GameObject bubbleField;
+
+    private PlayField playField;
 
     void Awake()
     {
@@ -56,19 +58,33 @@ public class GameManager : MonoBehaviour
         if (File.Exists(Application.dataPath + "/gamedata.json"))
         {
             string gameSaveString = File.ReadAllText(Application.dataPath + "/gamedata.json");
-            PlayField playField = JsonConvert.DeserializeObject<PlayField>(gameSaveString);
+            playField = JsonConvert.DeserializeObject<PlayField>(gameSaveString);
         }
     }
     public void GeneratePlayField()
     {
         //GameObject playField = Resources.Load("Prefabs/PlayField/PlayField") as GameObject;
         //GameObject bubbleField = Resources.Load("Prefabs/PlayField/BubbleField") as GameObject;
+
         //Instantiate()
         //
         //GameObject bubbleFieldInScene =  (GameObject)Instantiate(bubbleField, playField.position, playField.rotation);
         //GameObject bubbleFieldInScene = (GameObject)Instantiate(bubbleField);
         //bubbleFieldInScene.transform.parent = playField.transform;
+        GameObject bubbleFieldInScene = Instantiate(bubbleField, playFieldTransform);
+        foreach (var bubbleRow in playField.bubbleField.bubbleRows)
+        {
+            GameObject bubbleRowPrefab = Resources.Load("Prefabs/PlayField/BubbleRow") as GameObject;
+            //bubbleRowPrefab.transform.SetParent(bubbleFieldInScene.transform);
+            GameObject bubbleRowInScene = Instantiate(bubbleRowPrefab, bubbleFieldInScene.transform.position, bubbleField.transform.rotation);
+            bubbleRowInScene.transform.SetParent(bubbleFieldInScene.transform);
 
-        Instantiate(bubbleField, playField);
+            foreach (var goalBubble in bubbleRow.goalBubbles)
+            {
+                GameObject goalBubblePrefab = Resources.Load("Prefabs/Balls/GoalBubble") as GameObject;
+                GameObject goalBubbleInScene = Instantiate(goalBubblePrefab, bubbleRowInScene.transform.position, bubbleRowInScene.transform.rotation);
+                goalBubbleInScene.transform.SetParent(bubbleRowInScene.transform);
+            }
+        }
     }
 }
